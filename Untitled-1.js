c4b77084eb49c28f74f4cf6cb69120d6531c295b
Ashwin-Developer <88386942+Ashwin-Developer@users.noreply.github.com>
2021-08-03 19:08:48 +0530

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,15 @@
+
+
+let a = 3;
+let b = 15;
+let addtion = a + b;
+let subtractions = a - b;
+let multiplication = a * b;
+let division = b / a;
+let remainder = b % a;
+
+console.log("Addition ",$(addtion));
+console.log("Addition ",$(subtractions));
+console.log("Addition ",$(multiplication));
+console.log("Addition ",$(division));
+console.log("Addition ",$(remainder));
